79e0757137064665b8538fe23d513cd22346792a
mona atighi <monaatighi97@gmail.com>
2023-11-01 18:30:36 +0330

    update about page

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/sass/Pages/contact.scss">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css ">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/fontawesome-min.css">
- 
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
   
 
   <!--<script href="/wp-content/themes/theme-init/theme-init/assets/js/dist/bootstrap.bundle.min.js" />-->
@@ -24,9 +24,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
      <input type="text" class="bt-sm" >
     </button>
-    <form  >
-     <input type="text"></input>
-    </form>
+    
     <div class="collapse navbar-collapse right " id="collapsibleNavbar">
      <ul class="navbar-nav">
         <li class="nav-item">
@@ -50,6 +48,13 @@
       </ul>
     
     </div>
+    <div class="btn bg-info radius-6 d-block">
+     
+        <p color="text-white">0042626046</p>
+    </div>
+    <form class="d-flex">
+        <input class="form-control btn-sm bg-dark radius-6" type="text" placeholder="Search">
+      </form>
   </div>
 </nav>
 </header>
--- templates/about.php
+++ templates/about.php
@@ -4,11 +4,11 @@
 
 /*Template Name: About Page */ ?>
 <?php get_header(); ?>
-<main  class="contact-us-page  col-sm-12">
-<div class="container ">
-   <div class="our-story">
-      <h1>our story</h1>
-      <p>Welcome to Yooz Move, where we are dedicated to serving as your trusted partner for all your moving 
+<main  class="contact-us-page  w-100">
+<div class="container-fluid ">
+   <div class="our-story left">
+      <h1>Our Story</h1>
+      <p class="text-white justify text-left">Welcome to Yooz Move, where we are dedicated to serving as your trusted partner for all your moving 
          requirements in the DFW area. Our company was established by Nader Sianaki, a seasoned entrepreneur 
          renowned for his extensive expertise in logistics and operations. At Yooz Move, we prioritize delivering 
          dependable and efficient moving services customized to meet your specific needs. With our commitment to excellence 
@@ -16,19 +16,75 @@
       </p>
    </div>
    <div class="about1">
-      <div class="left">
-         <img src="" />
-      </div>
-      <div class="right content-about">
-         <p>Nader Sianaki brings a wealth of expertise to the moving industry. With a Master of Business 
+      
+      <div class=" right text-white justify text-left">
+         <p class="w-80 m-5">Nader Sianaki brings a wealth of expertise to the moving industry. With a Master of Business 
             Administration degree from Melbourne Business School (Australia) and a Bachelor of Software 
             Engineering with Honours from the University of Auckland 
             (New Zealand), Nader combines business acumen with a deep understanding of technology and operations.
          </p>
       </div>
-
+      <div class="left">
+      <img src="<?= get_stylesheet_directory_uri() .'/assets/img/about1.svg' ?>"/>
+      </div>
       </div>
    </div>
+   <div class="about1">
+     <div class="left">
+      <img src="<?= get_stylesheet_directory_uri() .'/assets/img/about1.svg' ?>"/>
+     </div>
+     <div class=" right text-white justify text-left">
+         <p class="w-80 m-5">Nader Sianaki brings a wealth of expertise to the moving industry. With a Master of Business 
+            Administration degree from Melbourne Business School (Australia) and a Bachelor of Software 
+            Engineering with Honours from the University of Auckland 
+            (New Zealand), Nader combines business acumen with a deep understanding of technology and operations.
+         </p>
+      </div>
+      
+   </div>
+   <div class="about1">
+      
+      <div class=" right text-white justify text-left">
+         <p class="w-80 m-5">Nader Sianaki brings a wealth of expertise to the moving industry. With a Master of Business 
+            Administration degree from Melbourne Business School (Australia) and a Bachelor of Software 
+            Engineering with Honours from the University of Auckland 
+            (New Zealand), Nader combines business acumen with a deep understanding of technology and operations.
+         </p>
+      </div>
+      <div class="left">
+      <img src="<?= get_stylesheet_directory_uri() .'/assets/img/about1.svg' ?>"/>
+      </div>
+      </div>
+   </div>
+   <br/>
+   <div class="our-team left">
+      <h1>Our Team</h1>
+      <p class="text-white justify text-left">At Yooz Move, we take pride in our dedicated and skilled 
+         team of professionals who are passionate about providing exceptional moving services to our valued customers
+          in the DFW area. With a commitment to excellence and a customer-centric approach,
+          our team works tirelessly to ensure a seamless and stress-free moving experience for every client.
+      </p>
+      <br/>
+      <div class="team">
+       <div class=" right text-white justify text-left">
+         <p class="w-80 m-5">Yooz Move team, chosen for expertise and professionalism, 
+            ensures a seamless and stress-free move. They're dedicated to exceeding expectations,
+             driven by core values and equipped to handle any moving challenge through ongoing training.
+              Team collaboration and diverse strengths result in customized moving solutions, prioritizing customer satisfaction.
+               Choose Yooz Move for reliable, efficient services,
+             making your move smooth and stress-free. Discover the Yooz Move difference with our exceptional team.
+         </p>
+         <div class="counter"></div>
+      </div>
+      <div class="left">
+       <img src="<?= get_stylesheet_directory_uri() .'/assets/img/our-team.svg' ?>"/>
+       
+      </div>
+     </div>
+   </div>
+   
+   
+     
 </div>
       
         
@@ -36,3 +92,4 @@
          
       
 </main>
+<?php get_footer(); ?>

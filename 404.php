a2d399e7c79d8d7659df0aeaa9cff12faa1db0c7
mona atighi <monaatighi97@gmail.com>
2023-11-02 17:58:28 +0330

    update header-not complate

--- 404.php
+++ 404.php
@@ -8,9 +8,11 @@
     <div class="not-found ">
   <h1>Oops!</h1>
   <h4>page not found</h4>
-  <input class="" name="404" placeholder="Go to Home" />
+  <input class="btn-notfound" name="404" placeholder="Go to Home" />
   <br/>
-  <img src="<?= get_template_directory_uri();?> '/assets/img/404.svg' "  alt="page not found" />
-</div>
+  </div>
+  <div class="img-not-found">
+  <img src="<?= get_template_directory_uri();?> '/assets/img/not.svg' "  alt="page not found" />
+  </div>
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/sass/Pages/contact.scss">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css ">
+  <!-- <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css "> -->
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/fontawesome-min.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
   
@@ -13,50 +13,39 @@
   <!--<script href="/wp-content/themes/theme-init/theme-init/assets/js/dist/bootstrap.bundle.min.js" />-->
 
 	<?php wp_head() ?>
-
+<?php 
+$phone = get_field('phone');
+?>
 </head>
 
 <body <?php body_class() ?> >
-<header>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-     <input type="text" class="bt-sm" >
-    </button>
-    
-    <div class="collapse navbar-collapse right " id="collapsibleNavbar">
-     <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Additional service</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
-        </li>   
-         <li class="nav-item">
-          <a class="nav-link" href="#">contact</a>
-        </li> 
-         <li class="nav-item">
-          <a class="nav-link" href="#">Aboutus</a>
-        </li>
-      </ul>
-    
-    </div>
-    <div class="btn bg-info radius-6 d-block">
-     
-        <p color="text-white">0042626046</p>
-    </div>
-    <form class="d-flex">
-        <input class="form-control btn-sm bg-dark radius-6" type="text" placeholder="Search">
-      </form>
-  </div>
-</nav>
+<header class="site-header">
+<div class="container desktop-header">
+			<div class="header-content">
+				
+             <!-- <div class="logo"><?php the_custom_logo() ?></div> -->
+             
+          <form action="/">
+							<input class="search" type="search" placeholder="search"
+								value="<?php the_search_query(); ?>" name="s" id="search" />
+					</form>
+          <div class="phone"><p>0042424242</p></div>
+          <div class="desktop-menu">
+
+					    <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+             </ul>
+             </div>
+          <div class="logo"><img src="/logo.svg"  /></div>
+				
+
+				
+			</div>
+		</div>
 </header>
 
 	<?php wp_body_open() ?>

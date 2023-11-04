1e1126cd6efeb22a35f54fff402584162e043d97
mona atighi <monaatighi97@gmail.com>
2023-11-04 23:00:28 +0330

    add sass file of card

--- comments.php
+++ comments.php
@@ -3,24 +3,22 @@
 
 /*Template Name: comment Page */ ?>
 <?php get_header(); ?>
-
-<div class="card card-comment">
-</div>
-<!-- show comment in page-->
-		<?php
+<?php
 
 $my_query = array("post_type" => "comment","posts_per_page" => 6);
 $query1 = new WP_Query($my_query);
 while($query1->have_posts()) : $query1->the_post();
 
 ?>
-<article class="comment">
+<div class="card card-comment">
+    <div class="card-header">
+        <h5><a href=”<?php the_permalink(); ?>”><?php the_title(); ?> </a></h5>
+    </div>
+    <div class="card-body">
+        <p><?php the_content(); ?></p>
+    </div>
+</div>
 
-<h3><a href=”<?php the_permalink(); ?>”><?php the_title(); ?> </a></h3>
-
-<div class=”img-product”><?php the_post_thumbnail(); ?></div>
-
-</article>
 
 <?php endwhile; wp_reset_postdata(); ?>
 
--- templates/about.php
+++ templates/about.php
@@ -361,7 +361,24 @@ $content_13 =get_field('content13');
 </div>
       
         
-        
+<?php
+
+$my_query = array("post_type" => "comment","posts_per_page" => 6);
+$query1 = new WP_Query($my_query);
+while($query1->have_posts()) : $query1->the_post();
+
+?>
+<div class="card card-comment">
+    <div class="card-header">
+        <h5><a href=”<?php the_permalink(); ?>”><?php the_title(); ?> </a></h5>
+    </div>
+    <div class="card-body">
+        <p><?php the_content(); ?></p>
+    </div>
+</div>
+
+
+<?php endwhile; wp_reset_postdata(); ?> 
          
       
 </main>

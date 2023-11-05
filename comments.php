2ac32d01b9115f791106f44e1db2981df37a94f2
mona atighi <monaatighi97@gmail.com>
2023-11-05 21:33:56 +0330

    update card

--- comments.php
+++ comments.php
@@ -12,7 +12,9 @@ while($query1->have_posts()) : $query1->the_post();
 ?>
 <div class="card card-comment">
     <div class="card-header">
-        <h5><a href=”<?php the_permalink(); ?>”><?php the_title(); ?> </a></h5>
+        <img  src="../wp-content/themes/theme-init/yoozmove/assets/img/search-logo.svg" />
+        <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h5>
+        <p> <? php echo current_time('H:i');?></p>
     </div>
     <div class="card-body">
         <p><?php the_content(); ?></p>

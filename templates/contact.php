c6e5a8cb1d17445068e221a3f9f4482f4237da12
mona atighi <monaatighi97@gmail.com>
2023-11-10 16:32:54 +0330

    add post  type and taxonomy
    add post  type and taxonomy
    update scss

--- templates/contact.php
+++ templates/contact.php
@@ -1,17 +1,18 @@
 <?php
 
-
-
-/*Template Name: Contact-us Page */ ?>
-<?php get_header();
-
 $title_location = get_field('location');
 $title_company = get_field('company_number');
 $title_contact = get_field('connect_with_us');
 
 
 
-?>
+
+
+
+/*Template Name: Contact-us Page */ ?>
+<?php get_header();?>
+
+
 <main  class="contact-us-page w-100">
     <div class="container ">
        <div class="contact-us">
@@ -39,18 +40,20 @@ $title_contact = get_field('connect_with_us');
        <form method="post" action="function.php">
         <h2 class="yellow">contact us</h2>
            <div class="form-group">
+           <input type="text" class="input-form w-50" name="lastname" placeholder="*LastName" />
               <input type="text" class="input-form w-50" name="firstname" placeholder="*FirstName" />
-              <input type="text" class="input-form w-50" name="lastname" placeholder="*LastName" />
+             
            </div>
            <div class="form-group">
+           <input type="email" class="input-form w-50" name="email" placeholder="*Email" />
               <input type="text" class="input-form w-50" name="number" placeholder="PhoneNumber" />
-              <input type="email" class="input-form w-50" name="email" placeholder="*Email" />
+              
            </div>
-           <div class="form-group">
-           <textarea rows="5" class="input-form w-100 " name="message" placeholder="*Message"></textarea>
+           <div class="form-group ">
+           <textarea row="10"  class="input-form left " name="message" placeholder="*Message"></textarea>
 
            </div>
-           <div class="">
+           <div class="form-group">
                 <input type="submit" value="send message" name="submit" class="submit-form"/>
             </div>
        </form>

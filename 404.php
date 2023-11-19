5f1dd755fc4d8d3ea4f80f8f289000fe3f7b47a4
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2023-11-19 17:32:21 +0330

    page 404 completed

--- 404.php
+++ 404.php
@@ -1,9 +1,14 @@
 <?php get_header() ?>
 <main class="not-found-page">
-    <section>
-        <p>oops!</p>
-        <p>page not found</p>
-        <div>Go to home</div>
+    <section class="container">
+        <p class="title">oops!</p>
+        <p class="sub-title">page not found</p>
+        <a href="/">
+            <div class="button">Go to home</div>
+        </a>
+        <div class="image-not-found">
+            <img src="<?= get_stylesheet_directory_uri() . '/assets/img/not-found.png' ?>" alt="not-found">
+        </div>
     </section>
 </main>
 

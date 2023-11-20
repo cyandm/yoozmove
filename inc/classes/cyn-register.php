e0629176de39f9023b3b113f0ff5f1f1499daf28
ElaheMahmoodi <elahemahmoodi81@gmail.com>
2023-11-20 17:33:55 +0330

    faq taxonomy , post type , page created

--- inc/classes/cyn-register.php
+++ inc/classes/cyn-register.php
@@ -7,6 +7,9 @@ if (!class_exists('cyn_register')) {
         function __construct()
         {
             add_action('init', [$this, 'cyn_register_contact_us_forms']);
+
+            add_action('init', [$this, 'register_faq_post_type']);
+            add_action('init', [$this, 'cyn_add_faq_cat_taxonomy']);
         }
 
         public function cyn_register_contact_us_forms()
@@ -38,6 +41,61 @@ if (!class_exists('cyn_register')) {
             ];
 
             return register_post_type($postType, $args);
+        }
+        public function register_faq_post_type()
+        {
+            $faq_labels = [
+                'name' => 'faq',
+                'singular_name' => 'question',
+                'add_new' => 'add question',
+                'add_new_item' => 'add new question',
+                'new_item' => 'new question',
+                'edit_item' => 'edit question',
+                'view_item' => 'view question',
+                'all_items' => 'all questions',
+                'search_items' => 'search question',
+                'not_found' => 'question not found',
+                'not_found_in_trash' => 'question not found'
+            ];
+
+            $faq_args = [
+                'labels' => $faq_labels,
+                'description' => 'faq custom post type.',
+                'public' => true,
+                'publicly_queryable' => true,
+                'show_ui' => true,
+                'show_in_menu' => true,
+                'query_var' => true,
+                'rewrite' => array('slug' => 'faq'),
+                'capability_type' => 'post',
+                'has_archive' => true,
+                'hierarchical' => false,
+                'menu_position' => 20,
+                'supports' => array('author', 'title', 'editor'),
+                'taxonomies' => array('faq-cat'),
+                'show_in_rest' => true,
+                'menu_icon' => 'dashicons-format-status',
+            ];
+
+            register_post_type('faq', $faq_args);
+        }
+
+        function cyn_add_faq_cat_taxonomy()
+        {
+            $labels = [
+                'name' => 'faq category'
+            ];
+
+            $args = [
+                'hierarchical' => true,
+                'labels' => $labels,
+                'show_ui' => true,
+                'show_admin_column' => true,
+                'query_var' => true,
+                'rewrite' => ['slug' => 'faq-cat'],
+            ];
+
+            register_taxonomy('faq-cat', ['faq'], $args);
         }
     }
 }
--- templates/faq.php
+++ templates/faq.php
@@ -1,2 +1,32 @@
-<?php 
+<?php
 /*Template Name: Faq Page */
+$faq_information1 = get_field('faq_information')['section1'];
+$faq_information2 = get_field('faq_information')['section2'];
+
+$faq_information_sec1 = null !== $faq_information1 ? $faq_information1 : 'empty';
+$faq_information_sec2 = null !== $faq_information2 ? $faq_information2 : 'empty';
+
+
+
+
+?>
+<?php get_header(); ?>
+
+<main class="faq-page">
+    <section>
+        <div></div>
+        <div>
+            <h1></h1>
+            <p></p>
+            <div></div>
+        </div>
+    </section>
+    <section>
+        <h2></h2>
+        <p></p>
+    </section>
+    <section>
+
+    </section>
+</main>
+<?php get_footer(); ?>
